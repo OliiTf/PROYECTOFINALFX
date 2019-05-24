0aeb06644f5a51a7c262763d367aa971cb06bffb
OliiTf <olinka.tf@gmail.com>
2019-05-24 01:05:28 -0500

    recepactualizado

--- src/sample/recepcionController.java
+++ src/sample/recepcionController.java
@@ -4,9 +4,14 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,14 +20,42 @@ public class recepcionController implements Initializable {
     @FXML
     Button BtnReportes, BtnConsultas, BtnGuardar, BtnEliminar, BtnNuevo;
     @FXML
-    TextField txtFolio, txtNoDoc;
+    TextField txtFolio, txtNoDoc, txtQuienRecibe,txtareaayuntamiento,
+            txtfirma, txtpuesto, txtdirigido, txtasunto, txtobservaciones;
     @FXML
-    DatePicker dpFechaDoc, dpFechaRecep;
+    DatePicker dpFechaDoc, dpFechaRecep, dpfechalimite, dpfechaentrega;
     @FXML
-    ComboBox<String> cmbTipo, cmbFormato;
+    ComboBox<String> cmbTipo, cmbFormato, cmbinstruccion, cmbprioridad, cmbinstitucion;
+    @FXML
+    CheckBox checkentregado;
+
+
+
+
+
+    public void showStage() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("recepcion.fxml"));
+        Stage st= new Stage();
+        st.setTitle("RECEPCION");
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("org/kordamp/bootstrapfx/bootstrapfx.css");
+        st.setScene(scene);
+        st.show();
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        cmbTipo.getItems().add(0, "Oficio");
+        cmbTipo.getItems().add(1, "Tarjeta Informativa");
+        cmbTipo.getItems().add(2, "Escrito personal");
+
+        cmbFormato.getItems().add(0,"Original");
+        cmbFormato.getItems().add(1, "Copia");
+
+
+
+
 
     }
 }
