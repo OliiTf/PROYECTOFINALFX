328837b905cc181a90eb3f9653dbc6f1fb360b9e
Omar40 <17030657@itcelaya.edu.mx>
2019-06-03 18:19:25 -0500

    Generacion de reportes

--- src/Reportes/ReportesController.java
+++ src/Reportes/ReportesController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import sample.GeneracionReporte;
+
 import sample.MySQLConnection;
 
 import java.io.File;
